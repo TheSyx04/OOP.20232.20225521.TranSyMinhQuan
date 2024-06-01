7976c9b42b70c29df4c5110f2b2ae551f67db081
TheSyx <minhquantsy@gmail.com>
2024-06-01 21:40:38 +0700

    Part 6

---
+++ Lab5/AimsProject/src/hust/soict/dsai/aims/screen/CartScreenController.java
@@ -0,0 +1,11 @@
+package hust.soict.dsai.aims.screen;
+
+import hust.soict.dsai.aims.media.Cart;
+
+public class CartScreenController {
+
+	public CartScreenController(Cart cart) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
